62c6b7b2959a28ba575354dc7eee9bb031234ca1
minjun0307 <rlaalswns080307@gmail.com>
2026-08-29 20:20:27 +0900

    08_bubble_array

--- week2/1. basic/08_bubble_sort.py
+++ week2/1. basic/08_bubble_sort.py
@@ -33,6 +33,10 @@ def bubble_sort(arr):
         정렬된 배열
     """
     n = len(arr)
+    for i in range(0,n-1):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j] 
     
     # TODO: 외부 반복문 - n-1번 반복
     # 각 패스마다 가장 큰 원소가 끝으로 이동
@@ -56,9 +60,18 @@ def bubble_sort_optimized(arr):
         정렬된 배열
     """
     n = len(arr)
+    swapped = False  # 교환 발생 여부
     
     for i in range(n):
-        swapped = False  # 교환 발생 여부
+         
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j] 
+            else:
+                swapped = True
+
+            if(swapped):
+                break
         
         # TODO: 내부 반복문과 교환 로직 구현
         # 교환이 발생하면 swapped = True 설정        
